4bc39badaa04b8cddb8baa759f230b3950604fdb
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-03-09 15:54:58 +0600

    connect to mongo db

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const port = 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// serviceMasterDB
+// yRmWlErAuGHXTkxM
+
+// middleware-----------
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://serviceMasterDB:yRmWlErAuGHXTkxM@cluster0.hhpkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // service related apis -------------
+     const service_DB =client.db('service_DB')
+     const serviceCollection = service_DB.collection("services")
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/",(req,res)=>{
     res.send("hello word")
