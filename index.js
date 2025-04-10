527d61c536af9b267a1927fb58bc0a0a4bcf5119
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-04-10 11:07:37 +0600

    get data by query email

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require("express")
 const cors = require("cors")
 
 const app = express()
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
- 
+
 
 // middleware-----------
 app.use(cors())
@@ -35,41 +35,49 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     // service related apis -------------
-     const service_DB =client.db('service_DB')
-     const serviceCollection = service_DB.collection("services")
-     const collectionOfBookedServices = service_DB.collection("bookedServices")
+    const service_DB = client.db('service_DB')
+    const serviceCollection = service_DB.collection("services")
+    const collectionOfBookedServices = service_DB.collection("bookedServices")
 
-     
-      app.get("/allData", async(req,res)=>{
-          const cursor=serviceCollection.find({})
-          const result = await cursor.toArray()
-          res.send(result)
-      })
-     app.get("/servicesForHomePage", async (req,res)=>{
-           const cursor =  serviceCollection.find({}).sort({_id:1}).limit(6)
-           const result =await cursor.toArray()
-           res.send(result)
-     })
-      app.get("/allData/:id", async (req,res)=>{
-              const id = req.params.id
-              const query = {_id : new ObjectId(id)}
-              const result = await serviceCollection.findOne(query)
-              res.send(result)
-              // console.log(id)
-      })
-     app.post("/service", async(req,res)=>{
-        const newService = req.body;
-        const result = await serviceCollection.insertOne(newService)
-        res.send(result)
-     })
+
+    app.get("/allData", async (req, res) => {
+      let query = {}
+      const email = req.query.email
+      if (email) {
+        query = { providerEmail: email }
+      }
+
+      const result = await serviceCollection.find(query).toArray()
+      res.send(result)
+    })
+    app.get("/servicesForHomePage", async (req, res) => {
+      const cursor = serviceCollection.find({}).sort({ _id: 1 }).limit(6)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.get("/allData/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await serviceCollection.findOne(query)
+      res.send(result)
+      // console.log(id)
+    })
+
+
+
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService)
+      res.send(result)
+    })
 
     //  bookedService related API
-     
-     app.post("/bookedServices", async(req,res)=>{
-              const bookedService = req.body;
-              const result = await collectionOfBookedServices.insertOne(bookedService)
-              res.send(result)
-     })
+
+    app.post("/bookedServices", async (req, res) => {
+      const bookedService = req.body;
+      const result = await collectionOfBookedServices.insertOne(bookedService)
+      res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -78,10 +86,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get("/",(req,res)=>{
-    res.send("hello word")
+app.get("/", (req, res) => {
+  res.send("hello word")
 })
 
-app.listen(port,()=>{
-    console.log(`service master is running on port${port}`)
+app.listen(port, () => {
+  console.log(`service master is running on port${port}`)
 })
